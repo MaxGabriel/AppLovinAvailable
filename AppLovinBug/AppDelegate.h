5105d62e9f159ab9fdd1f4a5775c343a640bee58
Maximilian Tagher <feedback.tagher@gmail.com>
2015-07-22 15:21:36 -0700

    Initial Commit

---
+++ AppLovinBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppLovinBug
+//
+//  Created by Maximilian Tagher on 7/22/15.
+//  Copyright (c) 2015 Heyzap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppLovinBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppLovinBug
+//
+//  Created by Maximilian Tagher on 7/22/15.
+//  Copyright (c) 2015 Heyzap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
